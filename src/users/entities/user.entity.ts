350d4facf046e1118cc211a14f0b11a1df82d766
therock24 <vitorlamela@gmail.com>
2025-03-21 11:27:31 +0000

    feat: start adding new user profile parameters, work in progress

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,8 +1,18 @@
 import { ApiHideProperty } from '@nestjs/swagger';
 
 export class User {
-  email: string;
-  @ApiHideProperty()
-  password: string;
+  id: string; // Unique identifier for the user
   name?: string | null;
+  email: string;
+  dateOfBirth?: Date | null; // Optional date of birth
+  avatar?: string | null; // Optional URL to the user's avatar
+  address?: {
+    street: string;
+    city: string;
+    state: string;
+    postalCode: string;
+    country: string;
+  } | null; // Optional address object
+  createdAt: Date; // Timestamp for when the user was created
+  updatedAt: Date; // Timestamp for when the user was last updated
 }
